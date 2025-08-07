06b9ed3c0bfe967a0dbb99bc033209e5d5d66319
Dewang Singh <dewang.singh@Dewangs-MacBook-Air.local>
2025-08-07 14:58:57 +0530

    conversion

---
+++ conversion&operations.js
@@ -0,0 +1,53 @@
+// let score = "23"
+
+// console.log(typeof(23));
+
+// //type coversion
+
+// let scoreInteger = Number(score)
+// console.log(scoreInteger);
+// console.log(typeof scoreInteger);
+
+
+
+// let score = "23qd"
+
+// console.log(typeof(23));
+
+// //type coversion
+
+// // let scoreInteger = Number(a)
+// console.log(scoreInteger);
+// console.log(typeof scoreInteger);
+
+
+// let a = "2"
+// // console.log(a);
+// // console.log(typeof a);
+
+
+// let scoreInteger = Boolean(a)
+// console.log(scoreInteger);
+// console.log(typeof scoreInteger);
+
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+// false =>0 
+
+
+
+//true = 1 flase = 0
+// "" = flase  
+// "ed3w" = true
+
+
+let number = 23
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
