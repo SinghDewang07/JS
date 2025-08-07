aa6154ad64af1aacdf58792896123c8f99c7b93e
Dewang Singh <dewang.singh@Dewangs-MacBook-Air.local>
2025-08-07 14:22:09 +0530

    data type

--- basic.js
+++ basic.js
@@ -7,3 +7,26 @@ let a = 7
 let b = 89
 
 console.log(a+b)
+
+
+// premitive Data type
+//number = 2 to power of 53
+// big int
+// string  = ""
+// boolean = true/ false
+// null = standalone value
+// undefined
+//sysmbol = unique
+
+
+
+// object
+
+
+console.log(typeof 23)
+console.log("The type of 23 is :", typeof 23)
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
