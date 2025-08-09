53427adf90ca585cae10629c78273ad048f280f0
Dewang Singh <dewang.singh@Dewangs-MacBook-Air.local>
2025-08-09 07:09:12 +0530

    string and their operations

---
+++ string.js
@@ -0,0 +1,26 @@
+let userName = "Dewang"
+let greet = "Good Evening"
+
+console.log(`Hello ${userName} how are u ${greet}!`);
+
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName);
+console.log(userName.charAt(3));
+console.log(userName.indexOf('e'));
+
+
+
+let password = "   23444943  "
+console.log(password.length);
+
+console.log(password.trim().length);
+
+
+let url = "https://dewang.singh%20/Portfolio-Home"
+console.log(url.replace('%20', '-'));
+
+
+let myName = "Dewang Singh"
+console.log(myName.split('e'));   // Split work by spiting the string into sub string by using the single word match
+
