9333411166b3166abeda5a3d06addbc5b8afd344
Dewang Singh <dewang.singh@Dewangs-MacBook-Air.local>
2025-08-19 17:20:42 +0530

    nums&math

---
+++ numsAndMath.js
@@ -0,0 +1,19 @@
+// const score = 400
+// console.log(score);
+// console.log(typeof score);
+
+
+// const balance = new Number(1000)
+// console.log(balance);
+// console.log(typeof balance);
+
+
+console.log(Math.random);
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(((Math.random()*10)+1)));
+
+
+
+console.log(Math.floor(((Math.random()*10)+10)));
+
